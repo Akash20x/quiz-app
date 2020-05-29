b36fb6b017123e1593f787d9a92eebbb13cf87c2
Akash20x <46225357+Akash20x@users.noreply.github.com>
2020-05-29 22:37:32 +0530

    Add files via upload

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <div className="header">
+      <h1>Quiz</h1>
+      <p>This is a true or false quiz app that tests your knowledge about different categories. You are given 15 seconds for every question of 10 and you have to correctly answer 7 of them to proceed to the next level.</p>
+    </div>
+  )
+}
